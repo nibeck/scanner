5c7a015cd36248f034e404d205d3d0542e6be3a6
Mike Nibeck <mnib@loc.gov>
2016-07-06 15:39:53 -0400

    New file

---
+++ Scanner/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Scanner
+//
+//  Created by Mike Nibeck on 6/25/16.
+//  Copyright © 2016 Mike Nibeck. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// This is a test comment
+
+class Person {
+    
+    // MARK: Properties
+    var firstName: String = ""
+    var lastName: String = ""
+    var photo: UIImage?
+    var emailAddress: String = ""
+}
